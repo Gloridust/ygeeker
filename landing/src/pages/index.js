809718de74835f8dbf8a4d46e852c27ac84bb472
rivertwilight <wrj2014@126.com>
2023-09-09 22:28:31 +0800

    init

---
+++ landing/src/pages/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<h1 className="hero__title">{siteConfig.title}</h1>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div className={styles.buttons}>
+					<Link
+						className="button button--secondary button--lg"
+						to="/docs/intro"
+					>
+						Docusaurus Tutorial - 5min ⏱️
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default function Home() {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<body>
+			<nav>
+				<div>
+          <div className="leading">asdfasdf</div>
+          <div className="options">fasfdfasf</div>
+        </div>
+			</nav>
+			<br />
+			<br />
+			<br />
+			<section>
+				<HomepageHeader />
+				<main>
+					<HomepageFeatures />
+				</main>
+			</section>
+		</body>
+	);
+}
