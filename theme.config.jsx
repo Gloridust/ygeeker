bbbff332d5f8d9c741a3c76484f1672b9d03fc19
rivertwilight <wrj2014@126.com>
2023-09-23 14:11:51 +0800

    feat: cusomize navbar

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./src/components/Navbar";
+
 export default {
-    logo: <span>My Nextra Documentation</span>,
-    project: {
-      link: 'https://github.com/shuding/nextra'
-    }
-    // ... other theme options
-  }
+	logo: <span>My Nextra Documentation</span>,
+	project: {
+		link: "https://github.com/shuding/nextra",
+	},
+	navbar: {
+		component: Navbar,
+	},
+	// ... other theme options
+};
