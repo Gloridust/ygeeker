f6ef0cea1ea4943b3d89e4d759b463df6ecfd2e1
rivertwilight <wrj2014@126.com>
2023-09-23 15:06:39 +0800

    feat: header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "nextra-theme-docs";
 
 export default function Navbar({ children }) {
 	return (
-		<div className="fixed left-0 right-0 top-0 h-[60px] flex justify-center py-1">
-			<div className="min-w-[900px] rounded-lg flex bg-white px-8 py-1 items-center shadow-lg justify-between">
+		<div className="fixed left-0 right-0 top-0 h-[60px] flex justify-center py-1 z-10">
+			<div className="min-w-[996px] rounded-lg flex bg-white px-8 py-1 items-center shadow-lg justify-between">
 				<div>
 					<Link href="/">
 						<img width={36} src="./logo.svg"></img>
