e0499c3079042e3dc4faa15be0b754302f71435d
rivertwilight <wrj2014@126.com>
2023-09-24 21:07:24 +0800

    feat: cusomize theme

--- theme.tsx
+++ theme.tsx
@@ -1,10 +1,36 @@
+import Head from "next/head";
+import Navbar from "./src/components/Navbar";
+import Link from "next/link";
 import type { NextraThemeLayoutProps } from "nextra";
 
-export default function Layout({ children }: NextraThemeLayoutProps) {
+export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
+	const { title, frontMatter, headings ,pageMap} = pageOpts;
+
 	return (
-		<div>
-			<h1>My Theme</h1>
+		<>
+			<Head>
+				<title>{title}</title>
+				<meta name="og:image" content={frontMatter.image} />
+			</Head>
+			<Navbar />
+			{/* <h1>My Theme</h1>
+			Table of Contents:
+			<ul>
+				{headings.map((heading) => (
+					<li key={heading.value}>{heading.value}</li>
+				))}
+			</ul> */}
+			{pageMap.map((item) => {
+				if (item.kind === "MdxPage") {
+					return (
+						<Link key={item.name} href={item.route}>
+							{item.route}
+						</Link>
+					);
+				}
+				return null;
+			})}
 			<div style={{ border: "1px solid" }}>{children}</div>
-		</div>
+		</>
 	);
 }
