f0e3a2caef26551b083eacdc00c53b0acab4f214
Gloridust <Gloridust@gmail.com>
2023-09-24 21:56:14 +0800

    Revert "Merge branch 'main' of https://github.com/yungeeker/yungeeker"
    This reverts commit a5ed5f5d4f36632306618dd7529254a7e0661679, reversing
    changes made to a676185d88b52a9ac5f6acf2150e7718f8a10ce2.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "nextra-theme-docs";
 
 export default function Navbar({ children }) {
 	return (
-		<div className="fixed left-0 right-0 top-0 h-[80px] flex justify-center py-4 z-10">
+		<div className="fixed left-0 right-0 top-0 h-[60px] flex justify-center py-1 z-10">
 			<div className="min-w-[996px] rounded-lg flex bg-white px-8 py-1 items-center shadow-lg justify-between">
 				<div>
 					<Link href="/">
@@ -10,7 +10,7 @@ export default function Navbar({ children }) {
 					</Link>
 				</div>
 				<div className="">
-					<Link className="mr-2" href="/support">asdfasfd</Link>
+					<Link className="mr-2" href="/support">Support</Link>
 					<Link href="/support">Join</Link>
 				</div>
 			</div>
--- theme.tsx
+++ theme.tsx
@@ -1,36 +1,10 @@
-import Head from "next/head";
-import Navbar from "./src/components/Navbar";
-import Link from "next/link";
 import type { NextraThemeLayoutProps } from "nextra";
 
-export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
-	const { title, frontMatter, headings ,pageMap} = pageOpts;
-
+export default function Layout({ children }: NextraThemeLayoutProps) {
 	return (
-		<>
-			<Head>
-				<title>{title}</title>
-				<meta name="og:image" content={frontMatter.image} />
-			</Head>
-			<Navbar />
-			{/* <h1>My Theme</h1>
-			Table of Contents:
-			<ul>
-				{headings.map((heading) => (
-					<li key={heading.value}>{heading.value}</li>
-				))}
-			</ul> */}
-			{pageMap.map((item) => {
-				if (item.kind === "MdxPage") {
-					return (
-						<Link key={item.name} href={item.route}>
-							{item.route}
-						</Link>
-					);
-				}
-				return null;
-			})}
+		<div>
+			<h1>My Theme</h1>
 			<div style={{ border: "1px solid" }}>{children}</div>
-		</>
+		</div>
 	);
 }
