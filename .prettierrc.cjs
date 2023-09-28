2e5e7a466d072e96a14ff3fbad335ff012449c48
jsun969 <i@jsun.lol>
2023-09-28 21:34:30 +0800

    chore(prettier): add prettier class sort plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,7 +2,10 @@ module.exports = {
 	tabWidth: 4,
 	bracketSpacing: true,
 	useTabs: true,
-	plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
+	plugins: [
+		require.resolve("@trivago/prettier-plugin-sort-imports"),
+		require.resolve("prettier-plugin-tailwindcss"),
+	],
 	importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
 	importOrderSeparation: true,
 };
