b183b3105b8dc2812f1bb42e4fa27c0f3285ae9d
jsun969 <i@jsun.lol>
2023-09-28 21:35:55 +0800

    style: format with prettier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,17 @@ import { Link } from "nextra-theme-docs";
 
 export default function Navbar({ children }) {
 	return (
-		<div className="fixed left-0 right-0 top-0 h-[60px] flex justify-center py-1 z-10">
-			<div className="min-w-[996px] rounded-lg flex bg-white px-8 py-1 items-center shadow-lg justify-between">
+		<div className="fixed left-0 right-0 top-0 z-10 flex h-[60px] justify-center py-1">
+			<div className="flex min-w-[996px] items-center justify-between rounded-lg bg-white px-8 py-1 shadow-lg">
 				<div>
 					<Link href="/">
 						<img width={36} src="./logo.svg"></img>
 					</Link>
 				</div>
 				<div className="">
-					<Link className="mr-2" href="/support">Support</Link>
+					<Link className="mr-2" href="/support">
+						Support
+					</Link>
 					<Link href="/support">Join</Link>
 				</div>
 			</div>
