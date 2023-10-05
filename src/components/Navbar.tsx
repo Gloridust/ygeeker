264d8f843770755214f2ba99f01f0744843c23d6
rivertwilight <wrj2014@126.com>
2023-10-05 22:42:38 +0800

    feat: use custom theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "nextra-theme-docs";
+import React from "react";
 
-export default function Navbar({ children }) {
+export default function Navbar({}) {
 	return (
 		<div className="fixed left-0 right-0 top-0 z-10 flex h-[60px] justify-center py-1">
 			<div className="flex min-w-[996px] items-center justify-between rounded-lg bg-white px-8 py-1 shadow-lg">
--- theme.tsx
+++ theme.tsx
@@ -1,10 +1,16 @@
 import type { NextraThemeLayoutProps } from "nextra";
+import React from "react";
+
+import Navbar from "./src/components/Navbar";
 
 export default function Layout({ children }: NextraThemeLayoutProps) {
 	return (
-		<div>
-			<h1>My Theme</h1>
-			<div style={{ border: "1px solid" }}>{children}</div>
-		</div>
+		<>
+			<Navbar />
+			<div>
+				<h1>My Theme</h1>
+				<div style={{ border: "1px solid" }}>{children}</div>
+			</div>
+		</>
 	);
 }
