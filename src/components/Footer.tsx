3cbba106a5f7559b55d0ab53082524e19346dcac
rivertwilight <wrj2014@126.com>
2023-10-23 11:46:16 +0800

    feat: add iddin't page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,6 +36,23 @@ const CUSTOM_LINKS = [
 			</svg>
 		),
 	},
+	{
+		title: "Contact Us",
+		description: "Talk with our engineer and CEO",
+		color: "bg-orange-600",
+		href: "https://github.com/ygeeker",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="fill-white"
+				height="24"
+				viewBox="0 -960 960 960"
+				width="24"
+			>
+				<path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
+			</svg>
+		),
+	},
 ];
 
 const FOOT_LINKS = [
@@ -53,7 +70,7 @@ export default function Footer({}) {
 	return (
 		<section className="mt-2 flex justify-center">
 			<footer className="max-w-[1200px] p-10">
-				<div className="justify-between sm:flex">
+				<div className="sm:flex sm:justify-between">
 					<div className="h-full sm:mr-8 md:text-left">
 						<h2 className="mb-4 text-2xl font-bold">YGeeker</h2>
 						<p>
