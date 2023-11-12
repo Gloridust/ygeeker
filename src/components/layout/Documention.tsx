415e721bd7733a4413562a20cb946726c5273c8c
rivertwilight <wrj2014@126.com>
2023-11-12 10:31:53 +0800

    lint

--- src/components/layout/Documention.tsx
+++ src/components/layout/Documention.tsx
@@ -2,7 +2,6 @@ import ChapterTree from "@/components/FolderTree";
 import ArticleMdxProvider from "@/components/Typography/ArticleMdxProvider";
 import themeConfig from "@/theme.config";
 import useMeta from "@/utils/useMeta";
-import { MDXProvider } from "@mdx-js/react";
 import { useRouter } from "next/router";
 import React from "react";
 import { useState } from "react";
