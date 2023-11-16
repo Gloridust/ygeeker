e18f3cbb096a51f043403f77c889adb3ea6e7028
rivertwilight <wrj2014@126.com>
2023-11-16 11:18:02 +0800

    feat: new hero style

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			dropShadow: {
+				"3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+				"4xl": [
+					"0 35px 35px rgba(0, 0, 0, 0.25)",
+					"0 45px 65px rgba(0, 0, 0, 0.15)",
+				],
+			},
 			fontFamily: {
 				sans: ["var(--font-noto-sans-display)"],
 				koulen: ["var(--font-koulen)"],
