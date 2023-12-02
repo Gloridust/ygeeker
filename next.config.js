b81e470c2a7e937d02bc09c4ff89d14ee8bd5efb
rivertwilight <wrj2014@126.com>
2023-12-02 16:31:22 +0800

    feat: force use ssr

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ module.exports = withNextra({
 		locales: ["en-US", "zh-CN"],
 		defaultLocale: "en-US",
 	},
+	domains: [
+		{
+			domain: "ygeeker.com.cn",
+			defaultLocale: "zh-CN",
+		},
+	],
 });
 
 // If you have other Next.js configurations, you can pass them as the parameter:
